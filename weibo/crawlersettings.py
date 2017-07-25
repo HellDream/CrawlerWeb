b1d3df14bfc7e24faf3ac5ea9777e21866820376
HellDream <673598755@qq.com>
2017-07-25 18:34:00 +0800

    delete sensitive info

--- weibo/crawlersettings.py
+++ weibo/crawlersettings.py
@@ -22,48 +22,8 @@ URL_NUMBER = 0
 
 ACCOUNTS = [
     {
-        "username": "17187306796",
-        "password": "gf754963"
-    },
-    {
-        "username": "17096445090",
-        "password": "ms332508"
-    },
-    {
-        "username": "17187306702",
-        "password": "ms329164"
-    },
-    {
-        "username": "17096444714",
-        "password": "ms326291"
-    },
-    {
-        "username": "17000569876",
-        "password": "ms323034"
-    },
-    {
-        "username": "17075306916",
-        "password": "ms321361"
-    },
-    {
-        "username": "15174975680",
-        "password": "ms320424"
-    },
-    {
-         "username": "17187306774",
-         "password": "ms315900"
-    },
-    {
-        "username": "17133143407",
-        "password": "ms315217"
-    },
-    # {
-    #     "username": "15648572606",
-    #     "password": "ms332043"
-    # },
-    {
-        "username": "17000569301",
-        "password": "ms1234567"
+        "username": "**********",
+        "password": "*******"
     },
 ]
 
